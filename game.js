e6439cb96b6a6dd82ca0ff9a1b5d348d5153b765
Jun Shirasaka <>
2019-07-21 18:34:08 +0900

    no message

--- game.js
+++ game.js
@@ -2,20 +2,34 @@ var audioElem;
 var current_song = 'sample.mp3';
 reader = new FileReader();
 message = document.getElementById("message");
+var numSongs = song_files.length;
+console.log('num of songs : ', numSongs);
+
+function writeText(i, str){
+  if(i == 1){
+    document.getElementById('btn1').innerText = str;
+  } else if (i == 2){
+    document.getElementById('btn2').innerText = str;
+  } else if (i == 3){
+    document.getElementById('btn3').innerText = str;
+  } else if (i == 4){
+    document.getElementById('btn4').innerText = str;
+  }
+}
 
 function onStartClicked(){
-  // selectSong();
+  selectSong();
   startPlaying(current_song);
-  // startPlaying(song_files[1]);
+  //startPlaying(song_files[2]);
 }
 
 function onSelected(which){
   judge(which);
 }
 
-function startPlaying(song_name){
+function startPlaying(file_name){
   audioElem = new Audio();
-  audioElem.src = song_name;
+  audioElem.src = file_name;
   audioElem.play();
   console.log(song_titles[1]);
 }
@@ -25,10 +39,24 @@ function stopPlaying(){
 } */
 
 function selectSong(){
-  numSongs = Object.keys(song_obj).length;
-  console.log(numSongs);
-  current_song = song_obj.name0;
-  console.log(current_song);
+  current_num = getRandom(1, numSongs);
+  current_song = song_files[current_num];
+  console.log(current_num);
+
+  var correct = getRandom(1, 4);
+  var alreadyUsedNum = [correct];
+  for(var i=1; i<=4; i++){
+    if(i == correct){
+      writeText(i, song_titles[current_num]);
+    } else {
+      randomNum = getRandom(1, song_files.length);
+      while(randomNum in alreadyUsedNum){
+        randomNum = getRandom(1, song_files.length);
+      }
+      alreadyUsedNum.push(randomNum);
+      writeText(i, song_titles[randomNum]);
+    }
+  }
 }
 
 function judge(which){
--- song_data.js
+++ song_data.js
@@ -1,11 +1,13 @@
 var song_titles = [
+  'sample',
   'NEO THEORY FANTASY',
   'ラビリンス・レジスタンス',
   'Ambitious Eve (アンティーカ Ver.)'
 ];
 
 var song_files = [
-  '01 NEO THEORY FANTASY.m4a',
-  '02 ラビリンス・レジスタンス.m4a',
-  '03 Ambitious Eve (アンティーカ Ver.).m4a'
+  'sample.mp3',
+  '01 NEO THEORY FANTASY.mp3',
+  '02 ラビリンス・レジスタンス.mp3',
+  '03 Ambitious Eve (アンティーカ Ver.).mp3'
 ];
