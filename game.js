b8de98587afbbb0e4d9a127a547f3f0c51849390
Jun Shirasaka <>
2019-07-21 19:12:35 +0900

    no message

--- game.js
+++ game.js
@@ -1,9 +1,10 @@
 var audioElem;
 var current_song = 'sample.mp3';
-reader = new FileReader();
-message = document.getElementById("message");
+var correct_btn;
 var numSongs = song_files.length;
 console.log('num of songs : ', numSongs);
+var _nowPlaying = false;
+var _nowSelected = false;
 
 function writeText(i, str){
   if(i == 1){
@@ -18,57 +19,75 @@ function writeText(i, str){
 }
 
 function onStartClicked(){
-  selectSong();
-  startPlaying(current_song);
-  //startPlaying(song_files[2]);
+  if(!_nowPlaying){
+    _nowPlaying = true;
+    next();
+    selectSong();
+    startPlaying(current_num);
+  }
 }
 
-function onSelected(which){
-  judge(which);
+function onSelected(selected_btn){
+  if(!_nowSelected){
+    _nowSelected = true;
+    stopPlaying();
+    judge(selected_btn);
+    _nowPlaying = false;
+  }
 }
 
-function startPlaying(file_name){
+function startPlaying(file_num){
   audioElem = new Audio();
-  audioElem.src = file_name;
+  audioElem.src = song_files[file_num];
   audioElem.play();
-  console.log(song_titles[1]);
+  console.log(song_titles[file_num]);
 }
-/*
+
 function stopPlaying(){
   audioElem.pause();
-} */
+}
 
 function selectSong(){
-  current_num = getRandom(1, numSongs);
-  current_song = song_files[current_num];
-  console.log(current_num);
-
-  var correct = getRandom(1, 4);
-  var alreadyUsedNum = [correct];
+  current_num = getRandom(0, numSongs-1);
+  console.log('current num : ', current_num);
+  correct_btn = getRandom(1, 4);
+  console.log('correct button : ', correct_btn);
+  var alreadyUsedNum = [current_num];
   for(var i=1; i<=4; i++){
-    if(i == correct){
+    if(i == correct_btn){
       writeText(i, song_titles[current_num]);
     } else {
-      randomNum = getRandom(1, song_files.length);
-      while(randomNum in alreadyUsedNum){
-        randomNum = getRandom(1, song_files.length);
+      randomNum = getRandom(0, numSongs-1);
+      while(alreadyUsedNum.indexOf(randomNum) >= 0){ 
+        randomNum = getRandom(0, numSongs-1);
       }
       alreadyUsedNum.push(randomNum);
       writeText(i, song_titles[randomNum]);
     }
   }
+  console.log(alreadyUsedNum);
 }
 
-function judge(which){
-  if (which == correct){
+function judge(selected_btn){
+  if (selected_btn == correct_btn){
     correct();
+  } else {
+    incorrect();
   }
 }
 
 function correct(){
+  document.getElementById('message').innerText = '正解！';
+}
+
+function incorrect(){
+  document.getElementById('message').innerText = '残念！';
 }
 
 function next(){
+  document.getElementById('message').innerText = '';
+  _nowPlaying = false;
+  _nowSelected = false;
 }
 
 function getRandom(min, max){
