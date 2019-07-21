4b0fbc0f69414f1b96eaebbc455ec322789819bc
Jun Shirasaka <>
2019-07-21 11:59:47 +0900

    no message

--- game.js
+++ game.js
@@ -1,11 +1,48 @@
 var audioElem;
+var current_song = 'sample.mp3';
+reader = new FileReader();
+message = document.getElementById("message");
 
-function startGame(){
-  audioElem = new Audio();
-  audioElem.src = "sample.mp3";
-  audioElem.play();
+function onStartClicked(){
+  // selectSong();
+  startPlaying(current_song);
+  // startPlaying(song_files[1]);
 }
 
-function stopGame(){
+function onSelected(which){
+  judge(which);
+}
+
+function startPlaying(song_name){
+  audioElem = new Audio();
+  audioElem.src = song_name;
+  audioElem.play();
+  console.log(song_titles[1]);
+}
+/*
+function stopPlaying(){
   audioElem.pause();
-} 
+} */
+
+function selectSong(){
+  numSongs = Object.keys(song_obj).length;
+  console.log(numSongs);
+  current_song = song_obj.name0;
+  console.log(current_song);
+}
+
+function judge(which){
+  if (which == correct){
+    correct();
+  }
+}
+
+function correct(){
+}
+
+function next(){
+}
+
+function getRandom(min, max){
+  return Math.floor(Math.random()*(max - min + 1) + min);
+}
---
+++ song_data.js
@@ -0,0 +1,11 @@
+var song_titles = [
+  'NEO THEORY FANTASY',
+  'ラビリンス・レジスタンス',
+  'Ambitious Eve (アンティーカ Ver.)'
+];
+
+var song_files = [
+  '01 NEO THEORY FANTASY.m4a',
+  '02 ラビリンス・レジスタンス.m4a',
+  '03 Ambitious Eve (アンティーカ Ver.).m4a'
+];
